05932a3d557da0f4ea7c7a9390c11ba6c6e19db1
Daniel Okobia <87054930+duokobia@users.noreply.github.com>
2025-09-23 19:26:39 +0100

    feat: Connect server to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
+import { connectDB } from './database/db';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -14,6 +19,11 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Home Page!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Connect to DB and start server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
 });
+
+
